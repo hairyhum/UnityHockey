346c558aa4c5d86935f77dd9bb3efe0d65777933
Daniil Fedotov <hairyhum@gmail.com>
2018-12-27 17:10:07 +0400

    init

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Team
+{
+    Blue,
+    Orange
+}
+
+public class PlayerControl : MonoBehaviour {
+
+    public float AngularSpeed;
+    public float Speed;
+    public bool Controlled = false;
+    public Team Team = Team.Blue;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().centerOfMass = new Vector2(0.0f, 0.0f);
+        if(Team == Team.Blue){
+            GetComponent<SpriteRenderer>().color = Color.blue;
+        } else if (Team == Team.Orange) {
+            GetComponent<SpriteRenderer>().color = Color.yellow;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var toggleControl = Input.GetButtonDown(TeamControl("ToggleControl"));
+        if(toggleControl){
+            Controlled = !Controlled;
+        }
+
+	}
+
+    void FixedUpdate()
+    {
+        if (Controlled)
+        {
+            var move = Input.GetAxis(TeamControl("Vertical"));
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, move * Speed);
+
+            var rotate = Input.GetAxis(TeamControl("Horizontal"));
+            GetComponent<Rigidbody2D>().angularVelocity = rotate * AngularSpeed;
+        }
+    }
+
+    string TeamControl(string control){
+        switch(Team){
+            case Team.Blue:
+                return control + "_p1";
+            case Team.Orange:
+                return control + "_p2";
+            default:
+                throw new Exception("Team must be defined");
+        }
+    }
+}
---
+++ Assets/Puck.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+struct Score {
+    public int blue;
+    public int orange;
+}
+
+public class Puck : MonoBehaviour {
+
+    public GameObject BlueGate;
+    public GameObject OrangeGate;
+
+    Score score = new Score{blue = 0, orange = 0};
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log(col.gameObject.name);
+        if(col.gameObject == BlueGate){
+            score.blue++;
+            BlueGate.transform.Find("Score").GetComponent<TextMesh>().text = score.blue.ToString();
+        } else if(col.gameObject == OrangeGate)
+        {
+            score.orange++;
+            OrangeGate.transform.Find("Score").GetComponent<TextMesh>().text = score.orange.ToString();
+        }
+    }
+}
