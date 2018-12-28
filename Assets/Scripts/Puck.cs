ec694520d905d823a3031ad969c3e395c634464e
Daniil Fedotov <hairyhum@gmail.com>
2018-12-28 18:20:46 +0400

    Prefabbing some things

--- Assets/Scripts/Puck.cs
+++ Assets/Scripts/Puck.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 struct Score {
     public int blue;
@@ -12,28 +13,37 @@ public class Puck : MonoBehaviour {
     public GameObject BlueGate;
     public GameObject OrangeGate;
 
+    public Text BlueScore;
+    public Text OrangeScore;
+
     Score score = new Score{blue = 0, orange = 0};
 
     // Use this for initialization
     void Start () {
-        
+        UpdateScore();
 	}
+
+    void UpdateScore(){
+        BlueScore.text = score.blue.ToString();
+        OrangeScore.text = score.orange.ToString();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
+
+
     void OnCollisionEnter2D(Collision2D col)
     {
         Debug.Log(col.gameObject.name);
         if(col.gameObject == BlueGate){
             score.blue++;
-            BlueGate.transform.Find("Score").GetComponent<TextMesh>().text = score.blue.ToString();
         } else if(col.gameObject == OrangeGate)
         {
             score.orange++;
-            OrangeGate.transform.Find("Score").GetComponent<TextMesh>().text = score.orange.ToString();
         }
+        UpdateScore();
     }
 }
